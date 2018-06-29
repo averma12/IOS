ca603695a8b59aacbd2ed227a46ab901767895f4
Abhinav Verma <abhinav_win12@yahoo.co.in>
2018-06-30 00:08:12 +0530

    Add RxSwift project

---
+++ RxSwift-1.0/RxSwift-1.0/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RxSwift-1.0
+//
+//  Created by Abhinav Verma on 29/06/18.
+//  Copyright © 2018 com.abhinav. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
